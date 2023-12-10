2f11b533a46f5b74de9af80af96eb067e6e47fd6
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 05:57:01 -0500

    Update authentication.py
    git update authentication.py

--- creodamo_platform/authentication.py
+++ creodamo_platform/authentication.py
@@ -1,5 +1,19 @@
+import jwt
+from flask import request, jsonify
+from rate_limiter import RateLimiter
+from config import AppConfig
+
 class Authentication:
+
+    def __init__(self):
+        self.config = AppConfig()
+        self.rate_limiter = RateLimiter()
+        self.secret_key = self.config.get_setting('SECRET_KEY')
+
     def authenticate(self, token):
-        # Implement authentication logic
-        # Verify if the token is valid and return True or False accordingly
-        return True
+        if not self.rate_limiter.is_allowed(token):
+            raise Exception("Rate limit exceeded")
+        return jwt.decode(token, self.secret_key, algorithms=["HS256"])
+
+    def require_role(self, role):
+        pass
