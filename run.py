d5001b117b518f2208796387e96bc8baed62efe8
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 06:09:38 -0500

    Update run.py
    git update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from creodamo import CreoDAMO
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Run CreoDAMO Platform')
+    parser.add_argument('--debug', action='store_true', help='Run in debug mode')
+    args = parser.parse_args()
+
+    damo = CreoDAMO(debug=args.debug)
+    damo.start()
 
 if __name__ == '__main__':
-  damo = CreoDAMO()
-  damo.run()
+    main()
