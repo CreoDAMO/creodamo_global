96d0e64612b05c0312ee34c9b0dd81e8be481462
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 06:10:53 -0500

    Update setup.py
    git update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='creodamo',
-  version='0.1',
-  packages=find_packages()
+    name='creodamo',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'celery', 'flask', 'jwt', 'redis', 'requests', 'websockets'
+    ],
+    entry_points={
+        'console_scripts': [
+            'creodamo=run:main'
+        ]
+    }
 )
