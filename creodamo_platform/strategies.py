cdc0dc15d2d1b06583b89c1bd21e6a8b24518373
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 06:01:49 -0500

    Update strategies.py
    git add strategies.py file

---
+++ creodamo_platform/strategies.py
@@ -0,0 +1,10 @@
+class MomentumStrategy:
+
+    def __init__(self, parameters):
+        self.parameters = parameters
+
+    def backtest(self, historical_data):
+        return "Backtest result"
+
+    def execute(self, market_data):
+        return "Trade signals"
