9d6fff02e507597089938919bd6ba9e14b5603b0
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 07:05:55 -0500

    Create Invitation.py
    git add invitation.py

---
+++ creodamo_platform/Invitation.py
@@ -0,0 +1,35 @@
+from reciprocity_circles import ReciprocityCircles
+from autonomous_discovery import AutonomousDiscovery
+from harmonic_exchange import HarmonicExchange
+from ecological_interdependence import EcologicalInterdependence
+
+class InvitingProgress:
+
+    def __init__(self):
+        self.reciprocity = ReciprocityCircles()
+        self.autonomous = AutonomousDiscovery()
+        self.harmonic = HarmonicExchange()
+        self.ecological = EcologicalInterdependence()
+
+    def nurture_reciprocal_relationships(self):
+        self.reciprocity.weave_understanding()
+        self.reciprocity.co_create_with_care()
+
+    def cultivate_autonomous_discovery(self):
+        self.autonomous.reveal_diverse_visions()
+        self.autonomous.uplift_all_voices()
+
+    def engage_in_harmonic_exchange(self):
+        self.harmonic.host_story_circles()
+        self.harmonic.amplify_marginalized_gifts()
+
+    def honor_ecological_interdependence(self):
+        self.ecological.maintain_balance()
+        self.ecological.consider_all_beings()
+
+if __name__ == '__main__':
+    progress = InvitingProgress()
+    progress.nurture_reciprocal_relationships()
+    progress.cultivate_autonomous_discovery()
+    progress.engage_in_harmonic_exchange()
+    progress.honor_ecological_interdependence()
