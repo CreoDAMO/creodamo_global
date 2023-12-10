6a066b9220d49d448017df1d1b86b34f00b4e6cd
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 05:58:44 -0500

    Update celery_tasks.py
    git update celery_tasks.py

--- creodamo_platform/celery_tasks.py
+++ creodamo_platform/celery_tasks.py
@@ -1,9 +1,15 @@
 from celery import Celery
+import logging
+from config import AppConfig
 
-app = Celery("creodamo", broker="redis://localhost:6379/0")
+app_config = AppConfig()
+app = Celery("creodamo", broker=app_config.get_setting('CELERY_BROKER_URL'))
+logger = logging.getLogger(__name__)
 
-@app.task
-def process_task(data):
-    # Implement task processing logic
-    # Process the data asynchronously
-    return result
+@app.task(bind=True, max_retries=3, default_retry_delay=60)
+def process_task(self, data):
+    try:
+        return "Task completed"
+    except Exception as exc:
+        logger.error("Task failed, retrying...", exc_info=exc)
+        raise self.retry(exc=exc)
