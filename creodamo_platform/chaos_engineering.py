bc801f52216f6c226ece71866757829a9736b875
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 06:36:47 -0500

    Create chaos_engineering.py
    git add engineering.py

---
+++ creodamo_platform/chaos_engineering.py
@@ -0,0 +1,15 @@
+# chaos_engineering.py
+
+class ChaosEngineering:
+    def __init__(self):
+        self.tools = ['Gremlin', 'Chaos Toolkit']
+
+    def simulate_scenarios(self):
+        # Method to simulate different failure scenarios
+        pass
+
+    def organize_game_days(self):
+        # Method to organize game days for incident practice
+        pass
+
+    # Additional methods for chaos engineering practices
