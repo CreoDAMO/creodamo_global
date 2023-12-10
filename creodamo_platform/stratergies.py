2e0a0c454be4dd62b8bebe14854b753e543970fb
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 06:00:47 -0500

    Delete creodamo_platform/stratergies.py
    git delete strategies.py

--- creodamo_platform/stratergies.py
+++
@@ -1,5 +0,0 @@
-class MovingAverageStrategy:
-    def execute(self, market_data, config):
-        # Implement the strategy logic
-        # Analyze market_data and return trading signals or trades
-        return trades
