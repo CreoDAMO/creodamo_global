6206da8857e9391f6c846f1e6d2bdb264b90ad14
Jacque Antoine DeGraff <151800081+CreoDAMO@users.noreply.github.com>
2023-12-10 06:04:00 -0500

    Update utils.py
    git update utlis.py

--- creodamo_platform/utils.py
+++ creodamo_platform/utils.py
@@ -1,31 +1,23 @@
 import logging
-import json
+from config import AppConfig
 
 class Logger:
+
     def __init__(self):
-        # Initialize logger
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger('CreoDAMO')
 
     def info(self, message):
-        # Log info message
         self.logger.info(message)
 
     def error(self, message):
-        # Log error message
         self.logger.error(message)
 
 class ConfigValidator:
-    def __init__(self, file_path):
-        # Set config file path
-        self.file_path = file_path
 
-    def validate(self):
-        try:
-            # Validate and load configuration from file
-            with open(self.file_path, 'r') as file:
-                config = json.load(file)
-            # Add further validation logic if necessary
-            return config
-        except Exception as e:
-            raise ValueError(f"Config validation error: {e}")
+    def __init__(self):
+        self.config = AppConfig()
+        self.schema = self.config.get_setting('CONFIG_SCHEMA')
+
+    def validate(self, config):
+        return True
