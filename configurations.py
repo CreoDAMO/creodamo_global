9daf5ad5d379e786bb71adf91b0d429adefb8680
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-11 18:02:42 -0500

    Update configurations.py
    git update configurations.py

--- configurations.py
+++ configurations.py
@@ -1,5 +1,5 @@
-# configurations.py
-
+import os
+import dj_database_url
 from dynaconf import Dynaconf
 from ai_ml import AIConfigAnalyzer
 from iot import IoTConfigHandler
@@ -10,17 +10,33 @@ from ansible_manager import AnsibleManager
 from notification_system import NotificationSystem
 from dynamic_config_loader import DynamicConfigLoader
 from chaos_engine import ChaosEngine
+from controlplane import Metrics
+# Assuming celery_app is defined in your application
+from your_application import celery_app
 
 load_dotenv()
 
 class AppConfig:
 
+    metrics = Metrics()  # Initialize once and reuse
+
     def __init__(self):
+        # Load and parse environment variables
+        self.DEBUG = os.environ.get('DEBUG', False) 
+        self.DATABASE_URL = dj_database_url.parse(os.environ['DATABASE_URL'])
+        self.REDIS_URL = os.environ['REDIS_URL']
+
+        # Expose metrics for Control Plane
+        AppConfig.metrics.expose_metrics()
+
+        # Configuration for Dynaconf
         self.settings = Dynaconf(
             environments=True,
             load_dotenv=True,
             settings_files=['settings.yaml', '.secrets.yaml']
         )
+
+        # Initialize other components
         self.ai_analyzer = AIConfigAnalyzer()
         self.iot_handler = IoTConfigHandler()
         self.plugin_store = PluginStore()
@@ -31,5 +47,10 @@ class AppConfig:
         self.dynamic_config_loader = DynamicConfigLoader()
         self.chaos_engine = ChaosEngine()
 
+        # Configure Celery for Redis
+        celery_app.conf.broker_url = self.REDIS_URL
+
     def get_setting(self, key):
         return self.settings.get(key)
+
+# Additional configurations for logging, request tracing, etc. should be added here
