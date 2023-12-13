cdf80b36aae0865ae3ae2611788eb32f74404924
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 16:30:23 -0500

    Update creodamo.py
    git creodamo.py

--- creodamo_platform/creodamo.py
+++ creodamo_platform/creodamo.py
@@ -1,54 +1,57 @@
 # creodamo.py
 
+import argparse
 from blockchain_integration import BlockchainService
 from ai_ml_services import EthicalAIML
 from cloud_services import DecentralizedCloudService
-from security_framework import CryptoSecurityManager  # Updated class name
+from security_framework import CryptoSecurity
 from community_engagement import CommunityEngagementPlatform
 from regulatory_compliance import RegulatoryComplianceManager
-from security_pipeline import SecurityPipeline  # Correct class name
-from feature_flags import FeatureFlags
-from chaos_engineering import ChaosEngineering
-from incident_response import IncidentResponse
+from security_pipeline import SecurityPipeline
 
 class CreoDAMO:
-    def __init__(self, debug=False):
-        # Initialize all core and additional services
+    """
+    Main class for the CreoDAMO application, responsible for initializing
+    and managing all services.
+    """
+    def __init__(self, debug: bool = False):
+        """
+        Initialize the CreoDAMO application.
+
+        Args:
+            debug (bool): If True, the application runs in debug mode.
+        """
         self.debug = debug
         self.blockchain_service = BlockchainService()
         self.ai_ml_service = EthicalAIML()
         self.cloud_service = DecentralizedCloudService()
-        self.security_manager = CryptoSecurityManager()  # Updated instantiation
+        self.security_manager = CryptoSecurity()
         self.community_platform = CommunityEngagementPlatform()
         self.compliance_manager = RegulatoryComplianceManager()
-        self.security_pipeline = SecurityPipeline()  # Already correct
-        self.feature_flags = FeatureFlags()
-        self.chaos_engineering = ChaosEngineering()
-        self.incident_response = IncidentResponse()
+        self.security_pipeline = SecurityPipeline()
 
     def start_services(self):
-        # Start and manage all integrated services
+        """
+        Start and manage all integrated services of the application.
+        """
         self.blockchain_service.initialize()
         self.ai_ml_service.maintain_transparency_in_models()
         self.cloud_service.deploy_to_ipfs()
-        self.security_manager.implement_tls_encryption()  # Use appropriate start method
+        self.security_manager.implement_tls_encryption()
         self.community_platform.develop_community_voting_portal()
         self.compliance_manager.monitor_fintech_regulations()
-        self.security_pipeline.integrate_security_checks()  # Use appropriate start method
-        if self.feature_flags.rollout_feature('new_feature', 50):
-            # Launch new feature at 50% user base
-            pass
-        self.chaos_engineering.simulate_scenarios()
-        self.incident_response.handle_incident("Sample Incident")
+        self.security_pipeline.integrate_security_checks()
+        # Additional service startup logic...
 
     def start(self):
-        # Additional logic for starting the platform can be added here
+        """
+        Start the CreoDAMO application, initializing all services.
+        """
         if self.debug:
             print("Starting CreoDAMO in debug mode...")
         self.start_services()
 
 if __name__ == "__main__":
-    import argparse
     parser = argparse.ArgumentParser(description='Run CreoDAMO Platform')
     parser.add_argument('--debug', action='store_true', help='Run in debug mode')
     args = parser.parse_args()
