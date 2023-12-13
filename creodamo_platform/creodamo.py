a40f889b6359a763f141898c2840c65302690881
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 10:52:12 -0500

    Update creodamo.py
    git update creodamo.py

--- creodamo_platform/creodamo.py
+++ creodamo_platform/creodamo.py
@@ -3,12 +3,12 @@
 from blockchain_integration import BlockchainService
 from ai_ml_services import EthicalAIML
 from cloud_services import DecentralizedCloudService
-from security_framework import SecurityManager
-from community_engagement import CommunityEngagementPlatform  # Updated class name
-from regulatory_compliance import RegulatoryComplianceManager  # Updated class name
-from security_pipeline import SecurityPipeline
+from security_framework import CryptoSecurityManager  # Updated class name
+from community_engagement import CommunityEngagementPlatform
+from regulatory_compliance import RegulatoryComplianceManager
+from security_pipeline import SecurityPipeline  # Correct class name
 from feature_flags import FeatureFlags
-from chaos_engineering import ChaosEngineering  # Correct class name
+from chaos_engineering import ChaosEngineering
 from incident_response import IncidentResponse
 
 class CreoDAMO:
@@ -18,12 +18,12 @@ class CreoDAMO:
         self.blockchain_service = BlockchainService()
         self.ai_ml_service = EthicalAIML()
         self.cloud_service = DecentralizedCloudService()
-        self.security_manager = SecurityManager()
-        self.community_platform = CommunityEngagementPlatform()  # Updated instantiation
-        self.compliance_manager = RegulatoryComplianceManager()  # Updated instantiation
-        self.security_pipeline = SecurityPipeline()
+        self.security_manager = CryptoSecurityManager()  # Updated instantiation
+        self.community_platform = CommunityEngagementPlatform()
+        self.compliance_manager = RegulatoryComplianceManager()
+        self.security_pipeline = SecurityPipeline()  # Already correct
         self.feature_flags = FeatureFlags()
-        self.chaos_engineering = ChaosEngineering()  # Already correct
+        self.chaos_engineering = ChaosEngineering()
         self.incident_response = IncidentResponse()
 
     def start_services(self):
@@ -31,14 +31,14 @@ class CreoDAMO:
         self.blockchain_service.initialize()
         self.ai_ml_service.maintain_transparency_in_models()
         self.cloud_service.deploy_to_ipfs()
-        self.security_manager.enforce_policies()
-        self.community_platform.develop_community_voting_portal()  # Use appropriate start method
-        self.compliance_manager.monitor_fintech_regulations()  # Use appropriate start method
-        self.security_pipeline.integrate_security_checks()
+        self.security_manager.implement_tls_encryption()  # Use appropriate start method
+        self.community_platform.develop_community_voting_portal()
+        self.compliance_manager.monitor_fintech_regulations()
+        self.security_pipeline.integrate_security_checks()  # Use appropriate start method
         if self.feature_flags.rollout_feature('new_feature', 50):
             # Launch new feature at 50% user base
             pass
-        self.chaos_engineering.simulate_scenarios()  # Use appropriate start method
+        self.chaos_engineering.simulate_scenarios()
         self.incident_response.handle_incident("Sample Incident")
 
     def start(self):
