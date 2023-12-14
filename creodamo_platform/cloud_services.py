376a4c3d862cce94e1aa8be5781f5e08e30db7f9
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 19:37:16 -0500

    Update cloud_services.py
    git update cloud_services.py

--- creodamo_platform/cloud_services.py
+++ creodamo_platform/cloud_services.py
@@ -3,9 +3,10 @@
 class DecentralizedCloudService:
     def __init__(self):
         # Initialization code for DecentralizedCloudService
+        pass
 
     def initialize(self):
         # Code to initialize the Decentralized Cloud Service
-        pass
+        pass  # Replace 'pass' with actual initialization code if needed
 
 # Additional methods as needed
