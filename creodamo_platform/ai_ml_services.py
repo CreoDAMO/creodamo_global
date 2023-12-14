aac6cc2fd08da0f112a2742e8179338a6c3fe4b6
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 19:33:30 -0500

    Update ai_ml_services.py
    git update ai_ml_services.py

--- creodamo_platform/ai_ml_services.py
+++ creodamo_platform/ai_ml_services.py
@@ -3,9 +3,10 @@
 class EthicalAIML:
     def __init__(self):
         # Initialization code for EthicalAIML
+        pass
 
     def initialize(self):
         # Code to initialize the Ethical AI/ML service
-        pass
+        pass  # You can replace 'pass' with the actual initialization code
         
 # Additional methods as needed
