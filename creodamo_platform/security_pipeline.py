232d3429c13040fa5c3f82289fc65829f8ab2f45
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 20:06:06 -0500

    Update security_pipeline.py

--- creodamo_platform/security_pipeline.py
+++ creodamo_platform/security_pipeline.py
@@ -1,10 +1,11 @@
 # security_pipeline.py
 
 class SecurityPipeline:
-       def __init__(self):
-           # Initialization code for SecurityPipeline
+    def __init__(self):
+        # Initialization code for SecurityPipeline
+        pass
 
-       def initialize(self):
-           # Code to initialize the Security Pipeline
-           pass
+    def initialize(self):
+        # Code to initialize the Security Pipeline
+        pass  # Use 'pass' as a placeholder if there's no specific code yet
     # Additional methods for security pipeline management
