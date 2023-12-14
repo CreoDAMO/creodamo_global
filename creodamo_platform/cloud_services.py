3c1a5913e97fd9966d8239d4cfbe501962a7bfb7
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 19:51:08 -0500

    Update cloud_services.py

--- creodamo_platform/cloud_services.py
+++ creodamo_platform/cloud_services.py
@@ -1,12 +1,11 @@
 # cloud_services.py
 
-class CryptoSecurityManager:
+class DecentralizedCloudService:
     def __init__(self):
-        # Initialization code for CryptoSecurityManager
+        # Initialization code for DecentralizedCloudService
         pass
 
     def initialize(self):
-        # Code to initialize the Crypto Security Manager
+        # Code to initialize the Decentralized Cloud Service
         pass  # Replace 'pass' with actual initialization code if needed
-
 # Additional methods as needed
