26339c231efd5480cadf1c2c4e134a211a67946a
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 20:03:11 -0500

    Update regulatory_compliance.py

--- creodamo_platform/regulatory_compliance.py
+++ creodamo_platform/regulatory_compliance.py
@@ -3,9 +3,9 @@
 class RegulatoryComplianceManager:
     def __init__(self):
         # Initialization code for RegulatoryComplianceManager
+        pass
 
     def initialize(self):
         # Code to initialize the Regulatory Compliance Manager
-        pass
-
+        pass  # Replace 'pass' with actual initialization code if needed
 # Additional methods as needed
