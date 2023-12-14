784ea5524eaf6cf062d52a45ca75d7339fc2965f
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-13 19:54:10 -0500

    Update security_framework.py

--- creodamo_platform/security_framework.py
+++ creodamo_platform/security_framework.py
@@ -3,9 +3,10 @@
 class CryptoSecurityManager:
     def __init__(self):
         # Initialization code for CryptoSecurityManager
+        pass
 
     def initialize(self):
         # Code to initialize the Crypto Security Manager
-        pass
+        pass  # Use 'pass' as a placeholder if there's no specific code yet
 
 # Additional methods as needed
