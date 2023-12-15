027b92cbc2dc5023612a37481682c031667749e4
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-14 22:37:49 -0500

    Update strategies.py
    git update strategies.py

--- creodamo_platform/strategies.py
+++ creodamo_platform/strategies.py
@@ -1,10 +1,13 @@
-class MomentumStrategy:
+class SimpleMovingAverageStrategy:
+    def __init__(self, window_size):
+        self.window_size = window_size
 
-    def __init__(self, parameters):
-        self.parameters = parameters
+    def calculate_moving_average(self, market_data):
+        # Calculate the moving average based on the window size and market data
+        pass
 
-    def backtest(self, historical_data):
-        return "Backtest result"
-
-    def execute(self, market_data):
-        return "Trade signals"
+    def generate_trading_signal(self, market_data):
+        moving_average = self.calculate_moving_average(market_data)
+        
+        # Generate trading signals based on the moving average and market data
+        pass
