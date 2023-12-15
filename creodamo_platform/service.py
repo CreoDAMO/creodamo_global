765f64620e331f5d7f51124d7cd6bdafd466b5ff
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-15 13:39:07 -0500

    Create service.py
    git add service.py

---
+++ creodamo_platform/service.py
@@ -0,0 +1,14 @@
+# service.py
+
+class Service:
+    def __init__(self):
+        # Initialize any necessary variables or resources here
+        pass
+
+    async def start(self):
+        # Implement the logic to start the service here
+        pass
+
+    async def stop(self):
+        # Implement the logic to stop the service here
+        pass
