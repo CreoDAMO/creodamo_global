81f6b71a337013a63198a3ef2143d5612b78f7fe
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-14 20:05:06 -0500

    Update garden_watering.py
    git update garden_watering.py

--- creodamo_platform/garden_watering.py
+++ creodamo_platform/garden_watering.py
@@ -1,33 +1,32 @@
 # garden_watering.py
 
 import datetime
-import sms 
+import time
+import sms
 import RPi.GPIO as GPIO
 
 PUMP_PIN = 17
 
 def initialize():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(PUMP_PIN, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PUMP_PIN, GPIO.OUT)
 
 def water_garden():
-  GPIO.output(PUMP_PIN, GPIO.HIGH)
-  datetime.sleep(5)
-  GPIO.output(PUMP_PIN, GPIO.LOW)
+    GPIO.output(PUMP_PIN, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(PUMP_PIN, GPIO.LOW)
 
 def main():
-  
-  initialize()
-  
-  while True:
+    initialize()
     
-    now = datetime.datetime.now()
-    
-    if now.hour == 7 and now.minute == 0:
-      water_garden()
-      sms.send("+15558675309", "Garden watered!")
-      
-    datetime.sleep(3600)
+    while True:
+        now = datetime.datetime.now()
+        
+        if now.hour == 7 and now.minute == 0:
+            water_garden()
+            sms.send("+15558675309", "Garden watered!")
+        
+        time.sleep(3600)
       
 if __name__ == '__main__':
-  main()
+    main()
