12a6f6705fe1552552fd8f2f9e3f06b02f66d294
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-16 15:27:24 -0500

    Update creodamo.py
    git update creodamo.py

--- creodamo_platform/creodamo.py
+++ creodamo_platform/creodamo.py
@@ -8,32 +8,31 @@ import sys
 from concurrent.futures import ProcessPoolExecutor
 from typing import Dict, Optional
 
-# Adjusting imports for relative paths
-from .service import Service
-from .blockchain_integration import BlockchainService
-from .cloud_services import DecentralizedCloudService
-from .community_engagement import CommunityEngagementPlatform
-from .creolang import CreoLang
-from .documentation import Documentation
-from .feature_flags import FeatureFlags
-from .garden_watering import GardenWatering
-from .governance import Governance
-from .incident_response import IncidentResponse
-from .internationalization import Internationalization
-from .kubernetes_deployment import KubernetesDeployment
-from .monitoring import Monitoring
-from .monetization import Monetization
-from .proof_of_creo import ProofOfCreo
-from .regulatory_compliance import RegulatoryComplianceManager
-from .security_framework import CryptoSecurityManager
-from .security_pipeline import SecurityPipeline
-from .service_mesh import ServiceMesh
-from .strategies import Strategies
-from .trading import Trading
-from .user_management import UserManagement
-from .utils import Utils
-from .ventures_fund import VenturesFund
-from .websocket import WebSocket
+from service import Service
+from blockchain_integration import BlockchainService
+from cloud_services import DecentralizedCloudService
+from community_engagement import CommunityEngagementPlatform
+from creolang import CreoLang
+from documentation import Documentation
+from feature_flags import FeatureFlags
+from garden_watering import GardenWatering
+from governance import Governance
+from incident_response import IncidentResponse
+from internationalization import Internationalization
+from kubernetes_deployment import KubernetesDeployment
+from monitoring import Monitoring
+from monetization import Monetization
+from proof_of_creo import ProofOfCreo
+from regulatory_compliance import RegulatoryComplianceManager
+from security_framework import CryptoSecurityManager
+from security_pipeline import SecurityPipeline
+from service_mesh import ServiceMesh
+from strategies import Strategies
+from trading import Trading
+from user_management import UserManagement
+from utils import Utils
+from ventures_fund import VenturesFund
+from websocket import WebSocket
 
 class CreoDAMO:
     def __init__(self, debug: bool = False) -> None:
@@ -43,40 +42,55 @@ class CreoDAMO:
         self.event_loop: Optional[asyncio.AbstractEventLoop] = None
 
     def initialize_services(self) -> Dict[str, Service]:
-        # Initialize and return a dictionary of service instances
-        # ... (initialization of services)
         return {
             "blockchain_service": BlockchainService(),
-            # ... (other services)
+            "decentralized_cloud_service": DecentralizedCloudService(),
+            "community_engagement_platform": CommunityEngagementPlatform(),
+            "creolang": CreoLang(),
+            "documentation": Documentation(),
+            "feature_flags": FeatureFlags(),
+            "garden_watering": GardenWatering(),
+            "governance": Governance(),
+            "incident_response": IncidentResponse(),
+            "internationalization": Internationalization(),
+            "kubernetes_deployment": KubernetesDeployment(),
+            "monitoring": Monitoring(),
+            "monetization": Monetization(),
+            "proof_of_creo": ProofOfCreo(),
+            "regulatory_compliance": RegulatoryComplianceManager(),
+            "crypto_security_manager": CryptoSecurityManager(),
+            "security_pipeline": SecurityPipeline(),
+            "service_mesh": ServiceMesh(),
+            "strategies": Strategies(),
+            "trading": Trading(),
+            "user_management": UserManagement(),
+            "utils": Utils(),
+            "ventures_fund": VenturesFund(),
+            "websocket": WebSocket(),
         }
 
     async def start_services(self) -> None:
-        # Start each service
-        # Add actual implementation here
-        pass  # Placeholder, replace with actual code
+        for service in self.services.values():
+            await service.start()
 
     async def stop_services(self) -> None:
-        # Stop each service
-        # Add actual implementation here
-        pass  # Placeholder, replace with actual code
+        for service in self.services.values():
+            await service.stop()
 
     async def start(self) -> None:
-        # Main application logic
-        # Add actual implementation here
-        pass  # Placeholder, replace with actual code
+        await self.start_services()
+        # Add your main application logic here
+        # ...
 
 def configure_logging(debug: bool) -> None:
     # Configure logging based on debug flag
-    # Add actual implementation here
-    pass  # Placeholder, replace with actual code
+    # ...
 
 def handle_signals(loop: asyncio.AbstractEventLoop) -> None:
     # Handle system signals for graceful shutdown
-    # Add actual implementation here
-    pass  # Placeholder, replace with actual code
+    # ...
 
 if __name__ == "__main__":
-    # Command-line interface setup
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true', help='Debug mode')
     args = parser.parse_args()
@@ -88,7 +102,7 @@ if __name__ == "__main__":
     handle_signals(loop)
 
     try:
-        asyncio.run(creodamo.start(), debug=args.debug)
+        loop.run_until_complete(creodamo.start())
     except KeyboardInterrupt:
         print("CreoDAMO stopped by user.")
         loop.run_until_complete(creodamo.stop_services())
