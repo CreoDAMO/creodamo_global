147912777cca5b1bada515d3f214fd4d3d7d7881
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-17 20:28:41 -0500

    Update ventures_fund.py
    git update ventures_fund.py

--- creodamo_platform/ventures_fund.py
+++ creodamo_platform/ventures_fund.py
@@ -1,13 +1,71 @@
 # ventures_fund.py
 
-class VenturesFund:
-    def __init__(self):
-        self.partnerships = []
-        self.investments = []
+import uvicorn
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from creopay import CreoPay
+from creoshowcase import CreoShowcase
+from creochain import CreoChain
+from creostack import CreoStack, SmartContract
+from security import authenticate_user, create_access_token, get_current_user
+import os
 
-    def form_partnership(self, partner):
-        self.partnerships.append(partner)
+# Initialize FastAPI app
+app = FastAPI()
 
-    def track_investments(self):
-        for investment in self.investments:
-            print(f"Tracking returns for {investment}")
+# Database connection setup
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Initialize CreoLang modules
+creo_pay = CreoPay()
+creo_showcase = CreoShowcase()
+creo_chain = CreoChain()
+creo_stack = CreoStack()
+
+# OAuth2 setup for authentication
+oauth2_scheme = Depends(get_current_user)
+
+# Database models
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+# Smart contract integration
+milestone_contract = SmartContract("MilestoneCompletion")
+bonus_payment_contract = SmartContract("BonusPayment")
+warrants_contract = SmartContract("WarrantsExercise")
+
+@app.post("/execute_contract")
+def execute_contract(contract_name: str, data: dict, current_user: User = Depends(oauth2_scheme)):
+    if contract_name == "MilestoneCompletion":
+        return milestone_contract.execute(data)
+    elif contract_name == "BonusPayment":
+        return bonus_payment_contract.execute(data)
+    elif contract_name == "WarrantsExercise":
+        return warrants_contract.execute(data)
+    else:
+        raise HTTPException(status_code=400, detail="Invalid contract name")
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+# Additional endpoints and logic for the VenturesFund platform
+# ...
+
+# Run the FastAPI app
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
