d54110f6cf430d29603033b07caef508493acc4a
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-18 10:52:53 -0500

    Update creodamo.py
    git update creodamo.py

--- creodamo_platform/creodamo.py
+++ creodamo_platform/creodamo.py
@@ -7,7 +7,7 @@ import signal
 from concurrent.futures import ProcessPoolExecutor
 from typing import Dict, Optional
 
-# Import modules from creodamo_modules package
+# Import all necessary modules
 from creodamo_modules import (
     BlockchainService,
     DecentralizedCloudService,
@@ -35,68 +35,104 @@ from creodamo_modules import (
     WebSocket,
     RealmOfCreo,
 )
-
-# Import creodamo_ecommerce module
-import creodamo_ecommerce
+from creodamo_ecommerce import CreoDAMOEcommerce
+from creodamo_persistence import (
+    SQLRepository,
+    NoSQLRepository,
+    FilesystemRepository,
+)
+from creodamo_validation import RequestValidator
+from creodamo_throttling import RequestThrottler
+from creodamo_feature_toggle import FeatureToggle
+from creodamo_deployment import DeploymentPipeline
+from creodamo_instrumentation import MetricsCollector, Logger
+from creodamo_load_testing import LoadTester
+from creodamo_admin_console import AdminConsole
+from collaboration import CollaborationModule  # Import the Collaboration module
 
 class CreoDAMO:
-    def __init__(self, debug: bool = False):
+    def __init__(self, debug: bool = False) -> None:
         self.debug = debug
         self.services: Dict[str, Service] = self.initialize_services()
         self.executor: Optional[ProcessPoolExecutor] = None
         self.event_loop: Optional[asyncio.AbstractEventLoop] = None
 
+        # Initialize persistence repositories
+        self.sql_repository = SQLRepository()
+        self.nosql_repository = NoSQLRepository()
+        self.filesystem_repository = FilesystemRepository()
+
+        # Initialize request validator middleware
+        self.request_validator = RequestValidator()
+
+        # Initialize request throttler middleware
+        self.request_throttler = RequestThrottler()
+
+        # Initialize feature toggle manager
+        self.feature_toggle = FeatureToggle()
+
+        # Initialize deployment pipeline
+        self.deployment_pipeline = DeploymentPipeline()
+
+        # Initialize instrumentation components
+        self.metrics_collector = MetricsCollector()
+        self.logger = Logger()
+
+        # Initialize load testing tool
+        self.load_tester = LoadTester()
+
+        # Initialize admin console
+        self.admin_console = AdminConsole()
+
+        # Initialize Collaboration Module
+        self.collaboration_module = CollaborationModule()
+
     def initialize_services(self) -> Dict[str, Service]:
-        return {
-            "blockchain_service": BlockchainService(),
-            "decentralized_cloud_service": DecentralizedCloudService(),
-            "community_engagement_platform": CommunityEngagementPlatform(),
-            "creolang": CreoLang(),
-            "documentation": Documentation(),
-            "feature_flags": FeatureFlags(),
-            "garden_watering": GardenWatering(),
-            "governance": Governance(),
-            "incident_response": IncidentResponse(),
-            "internationalization": Internationalization(),
-            "kubernetes_deployment": KubernetesDeployment(),
-            "monitoring": Monitoring(),
-            "monetization": Monetization(),
-            "proof_of_creo": ProofOfCreo(),
-            "regulatory_compliance": RegulatoryComplianceManager(),
-            "crypto_security_manager": CryptoSecurityManager(),
-            "security_pipeline": SecurityPipeline(),
-            "service_mesh": ServiceMesh(),
-            "strategies": Strategies(),
-            "trading": Trading(),
-            "user_management": UserManagement(),
-            "utils": Utils(),
-            "ventures_fund": VenturesFund(),
-            "websocket": WebSocket(),
-            "realm_of_creo": RealmOfCreo(),
-            "creodamo_ecommerce": creodamo_ecommerce.CreoDAMOEcommerce(),  # E-commerce module
+        services = {
+            # Initialize all other services
+            "creodamo_ecommerce": CreoDAMOEcommerce(),  # Initialize the e-commerce module
+            # ... other service initializations ...
         }
+        return services
 
-    async def start_services(self):
-        await asyncio.gather(*(service.start() for service in self.services.values()))
+    async def start_services(self) -> None:
+        await asyncio.gather(
+            *(asyncio.to_thread(service.start()) for service in self.services.values())
+        )
 
-    async def stop_services(self):
+    async def stop_services(self) -> None:
         await asyncio.gather(*(service.stop() for service in self.services.values()))
 
-    async def start(self):
+    async def start(self) -> None:
         await self.start_services()
         # Main application logic here...
 
-def configure_logging(debug: bool):
+    async def shutdown(self):
+        for service in self.services.values():
+            await service.close()
+        await asyncio.gather(*(service.join() for service in self.services.values()))
+
+def configure_logging(debug: bool) -> None:
     logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
 
-def handle_signals(loop: asyncio.AbstractEventLoop):
-    loop.add_signal_handler(signal.SIGINT, loop.stop)
-    loop.add_signal_handler(signal.SIGTERM, loop.stop)
+def handle_signals(loop: asyncio.AbstractEventLoop) -> None:
+    loop.add_signal_handler(signal.SIGINT, lambda signum, frame: asyncio.create_task(creodamo.shutdown()))
+    loop.add_signal_handler(signal.SIGTERM, lambda signum, frame: asyncio.create_task(creodamo.shutdown()))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true', help='Enable debug mode')
     args = parser.parse_args()
+
+    creodamo = CreoDAMO(debug=args.debug)
+    loop = asyncio.get_event_loop()
+    handle_signals(loop)
+
+    try:
+        loop.run_until_complete(creodamo.start())
+    except KeyboardInterrupt:
+        print("CreoDAMO stopped by user.")
+        loop.run_until_complete(creodamo.shutdown())    args = parser.parse_args()
 
     configure_logging(args.debug)
 
