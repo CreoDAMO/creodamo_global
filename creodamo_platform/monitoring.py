89636df6ae7ab6dafc6d9b85fc7cddef17592f40
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-18 19:44:15 -0500

    Update monitoring.py
    git update monitoring.py

--- creodamo_platform/monitoring.py
+++ creodamo_platform/monitoring.py
@@ -1,12 +1,46 @@
 # monitoring.py
 
-from prometheus_client import start_http_server
-import logging
-from logstash_async.handler import AsyncLogstashHandler
+import numpy as np
+import pandas as pd
+import numba as nb
+import multiprocessing
+from typing import List
+import cProfile
+import dask.dataframe as dd
 
-def setup_monitoring():
-    start_http_server(8000)
-  
-    logger = logging.getLogger('logstash')
-    logger.setLevel(logging.INFO)
-    logger.addHandler(AsyncLogstashHandler())
+# Numba JIT compilation for parallel execution
+@nb.njit(parallel=True)
+def calculate_sum(data: np.ndarray) -> np.float64:
+    return np.sum(data)
+
+# Optimize code using multiprocessing
+def optimize_code(data: np.ndarray, num_processes: int = None) -> np.float64:
+    if num_processes is None:
+        num_processes = multiprocessing.cpu_count()
+
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        chunks = np.array_split(data, num_processes)
+        results = pool.map(calculate_sum, chunks)
+    return np.sum(results)
+
+# Profiling function
+def profile_code(data: np.ndarray):
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    result = optimize_code(data)
+
+    profiler.disable()
+    profiler.print_stats()
+
+    return result
+
+if __name__ == "__main__":
+    # Read and process data
+    dask_df = dd.read_csv("data.csv")
+    data = dask_df.compute().to_numpy()
+
+    # Run the optimized code with profiling
+    optimized_result = profile_code(data)
+
+    print("Optimized Result:", optimized_result)
