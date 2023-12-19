e289d1cc7cef3eec486cad8b4e53ce4c090c2b05
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-19 13:10:54 -0500

    Update configurations.py
    git update configurations.py

--- configurations.py
+++ configurations.py
@@ -1,56 +1,71 @@
+# configuration.py
+
 import os
 import dj_database_url
 from dynaconf import Dynaconf
-from ai_ml import AIConfigAnalyzer
-from iot import IoTConfigHandler
-from plugin_store import PluginStore
+from dotenv import load_dotenv
+from creolang import CreoLangInterpreter
+from ansible_manager import AnsibleManager
 from cloud_services import CloudConfigManager
 from blockchain_logger import BlockchainLogger
-from ansible_manager import AnsibleManager
-from notification_system import NotificationSystem
 from dynamic_config_loader import DynamicConfigLoader
 from chaos_engine import ChaosEngine
 from controlplane import Metrics
-# Assuming celery_app is defined in your application
 from your_application import celery_app
-
-load_dotenv()
+from ci_cd_pipeline_manager import CICDPipelineManager
+from rbac_manager import RBACManager
+from centralized_logger import CentralizedLogger
+from siem_manager import SIEMManager
+from threat_intelligence_manager import ThreatIntelligenceManager
+from bug_bounty_platform import BugBountyPlatform
+from service_mesh_integration import ServiceMeshIntegration
+from canary_deployment_manager import CanaryDeploymentManager
+from grafana_dashboard_manager import GrafanaDashboardManager
+from secure_communication import SecureCommunication
+from audit_logger import AuditLogger
+from security_compliance import SecurityCompliance
 
 class AppConfig:
-
-    metrics = Metrics()  # Initialize once and reuse
-
     def __init__(self):
-        # Load and parse environment variables
-        self.DEBUG = os.environ.get('DEBUG', False) 
-        self.DATABASE_URL = dj_database_url.parse(os.environ['DATABASE_URL'])
-        self.REDIS_URL = os.environ['REDIS_URL']
+        load_dotenv()
 
-        # Expose metrics for Control Plane
-        AppConfig.metrics.expose_metrics()
+        self.DEBUG = os.getenv('DEBUG', False) 
+        self.DATABASE_URL = dj_database_url.parse(os.getenv('DATABASE_URL'))
+        self.REDIS_URL = os.getenv('REDIS_URL')
 
-        # Configuration for Dynaconf
         self.settings = Dynaconf(
             environments=True,
             load_dotenv=True,
             settings_files=['settings.yaml', '.secrets.yaml']
         )
 
-        # Initialize other components
-        self.ai_analyzer = AIConfigAnalyzer()
-        self.iot_handler = IoTConfigHandler()
-        self.plugin_store = PluginStore()
+        self.creolang = CreoLangInterpreter()
+        self.ansible_manager = AnsibleManager()
         self.cloud_manager = CloudConfigManager()
         self.blockchain_logger = BlockchainLogger()
-        self.ansible_manager = AnsibleManager()
-        self.notification_system = NotificationSystem()
         self.dynamic_config_loader = DynamicConfigLoader()
         self.chaos_engine = ChaosEngine()
+        self.metrics = Metrics()
+        self.ci_cd_pipeline_manager = CICDPipelineManager()
+        self.rbac_manager = RBACManager()
+        self.centralized_logger = CentralizedLogger()
+        self.siem_manager = SIEMManager()
+        self.threat_intelligence_manager = ThreatIntelligenceManager()
+        self.bug_bounty_platform = BugBountyPlatform()
+        self.service_mesh = ServiceMeshIntegration()
+        self.canary_deployment_manager = CanaryDeploymentManager()
+        self.grafana_dashboard_manager = GrafanaDashboardManager()
+        self.secure_communication = SecureCommunication()
+        self.audit_logger = AuditLogger()
+        self.security_compliance = SecurityCompliance()
 
-        # Configure Celery for Redis
         celery_app.conf.broker_url = self.REDIS_URL
 
     def get_setting(self, key):
         return self.settings.get(key)
 
-# Additional configurations for logging, request tracing, etc. should be added here
+    # Additional methods for each manager
+    # Security enhancements for each manager
+
+# Instantiate AppConfig
+app_config = AppConfig()
