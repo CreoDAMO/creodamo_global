f166586b239e141cc897df8d6ca33ad29bf10815
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-20 15:31:33 -0500

    Update static_analysis.py
    git update static_analysis.py

--- creodamo_platform/tests/static_analysis.py
+++ creodamo_platform/tests/static_analysis.py
@@ -1,10 +1,51 @@
 import pylint
 import radon
+import mypy
+import black
+import flake8
+import isort
+import bandit
 
-def lint():
-  pylint.run_pylint()
+class StaticAnalysis:
+    def __init__(self):
+        # Initialize any configuration or settings needed for the tools
 
-def complexity():
-  radon.analyze()
+    def lint_code(self):
+        # Run Pylint for code linting
+        pylint.run_pylint()
 
-# ... Additional functions for security, performance, etc.
+    def check_complexity(self):
+        # Analyze code complexity with Radon
+        radon.analyze()
+
+    def type_check(self):
+        # Run MyPy for static type checking
+        mypy.run_mypy()
+
+    def format_code(self):
+        # Format code using Black
+        black.format_code()
+
+    def check_style_conventions(self):
+        # Check code against PEP8 style conventions with Flake8
+        flake8.check_code_style()
+
+    def sort_imports(self):
+        # Sort and organize imports with isort
+        isort.organize_imports()
+
+    def scan_security_vulnerabilities(self):
+        # Run Bandit for detecting security vulnerabilities
+        bandit.run_security_scan()
+
+    # Additional functions for other static analysis aspects
+
+# Example usage
+static_analyzer = StaticAnalysis()
+static_analyzer.lint_code()
+static_analyzer.check_complexity()
+static_analyzer.type_check()
+static_analyzer.format_code()
+static_analyzer.check_style_conventions()
+static_analyzer.sort_imports()
+static_analyzer.scan_security_vulnerabilities()
