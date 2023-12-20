0ce17298aac5d8bb82ead59900577595ca53f511
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-20 18:42:48 -0500

    Update websocket.py
    git update websocket.py

--- creodamo_platform/websocket.py
+++ creodamo_platform/websocket.py
@@ -1,16 +1,90 @@
+# websocket.py
+
 import websockets
-from prometheus_client import Counter
-from config import AppConfig
+import json
+import os
+import jwt
+from prometheus_client import Counter, Histogram
+from creolang import CreoLangInterpreter
+from authentication import Authentication
+import logging
+import asyncio
+import rate_limiter
+from message_queue import MessageQueue
+import error_monitoring
+import hmac
+import schema_validator
+import apm
+
+class AppConfig:
+    def __init__(self):
+        # Load configuration from file or environment variables
+        self.host = os.getenv("WEBSOCKET_HOST", "localhost")
+        self.port = int(os.getenv("WEBSOCKET_PORT", 8765))
+        self.secret_key = os.getenv("SECRET_KEY", "mysecretkey")
 
 class WebSocketServer:
 
     def __init__(self):
         self.config = AppConfig()
         self.connections_counter = Counter('websocket_connections', 'Number of WebSocket connections')
+        self.message_processing_histogram = Histogram('message_processing_time', 'Time taken to process messages')
+        self.auth = Authentication()
+        self.message_queue = MessageQueue()
+        self.rate_limiter = rate_limiter.RateLimiter()
 
     async def handler(self, websocket, path):
+        user_id, valid = await self.authenticate(websocket)
+        if not valid:
+            return
+
         self.connections_counter.inc()
         try:
-            pass
+            async for message in websocket:
+                if self.rate_limiter.is_rate_limited(user_id):
+                    await websocket.send(json.dumps({"error": "Rate limit exceeded"}))
+                    continue
+
+                processed_message = await self.process_message(message, user_id)
+                await websocket.send(processed_message)
+        except websockets.ConnectionClosed:
+            logging.info(f"Connection closed: {user_id}")
         finally:
             self.connections_counter.dec()
+
+    async def authenticate(self, websocket):
+        token = await websocket.recv()
+        return self.auth.authenticate_user(token)
+
+    async def process_message(self, message, user_id):
+        # Validate message format using schema
+        if not schema_validator.validate_message(message):
+            return json.dumps({"error": "Invalid message format"})
+
+        # Add request/response logging
+        logging.info(f"Received message from {user_id}: {message}")
+
+        # Process message using CreoLang
+        response = self.execute_creolang_script(message)
+        
+        # Add APM tracing
+        apm.trace_message_processing(user_id, message)
+
+        return response
+
+    def execute_creolang_script(self, message):
+        # Implement the logic to execute the CreoLang script here
+        return json.dumps({"response": "Processed message"})
+
+    def signature_valid(self, message, signature):
+        # HMAC signature verification logic
+        return hmac.verify_signature(message, signature)
+
+# Example usage
+async def start_server():
+    server = WebSocketServer()
+    async with websockets.serve(server.handler, server.config.host, server.config.port):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(start_server())
