d18360e18f2268658dfbcafe417adc2715275927
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-20 12:03:05 -0500

    Update cloud_services.py
    git update cloud_services.py

--- creodamo_platform/cloud_services.py
+++ creodamo_platform/cloud_services.py
@@ -7,55 +7,69 @@ import cryptography
 from filecoin import FilecoinClient
 from caching import CacheLayer
 from metrics import MetricsLogger
+import asyncio
 
 class DecentralizedCloudService:
-    def __init__(self):
-        self.ipfs = ipfshttpclient.connect()
+    def __init__(self, ipfs_address='/ip4/127.0.0.1/tcp/5001', encryption_key=None):
+        self.ipfs = ipfshttpclient.connect(ipfs_address)
         self.filecoin = FilecoinClient()
-        self.creolang = CreoLangInterpreter()
+        self.creolang = CreoLangInterpreter(encryption_key)
         self.cache = CacheLayer()
         self.logger = MetricsLogger()
 
     def initialize(self):
-        config = self.creolang.execute("config.cl")
-        self.apply_config(config)
+        try:
+            config = self.creolang.execute("config.cl")
+            self.apply_config(config)
+        except Exception as e:
+            self.logger.log_error(f"Initialization error: {e}")
 
     def apply_config(self, config):
-        # Initialize service based on CreoLang config
+        # Implement configuration application logic
 
     async def store(self, data, path, use_filecoin=False):
-        encrypted = self.creolang.execute("encrypt.cl", data)
-        compressed = zlib.compress(encrypted)
-        hash = await self.ipfs.add(compressed)
-        if use_filecoin:
-            await self.filecoin.store(hash)
-        await self.indexer.index(hash, path)
+        try:
+            encrypted = self.creolang.execute("encrypt.cl", data)
+            compressed = zlib.compress(encrypted)
+            hash = await self.ipfs.add(compressed)
+            if use_filecoin:
+                await self.filecoin.store(hash)
+            await self.indexer.index(hash, path)
+        except Exception as e:
+            self.logger.log_error(f"Store operation failed: {e}")
 
     async def retrieve(self, path):
-        if self.cache.exists(path):
-            return self.cache.get(path)
+        try:
+            if self.cache.exists(path):
+                return self.cache.get(path)
 
-        hash = await self.indexer.lookup(path)
-        file = await self.ipfs.get(hash)
-        decompressed = zlib.decompress(file)
-        decrypted = self.creolang.execute("decrypt.cl", decompressed)
+            hash = await self.indexer.lookup(path)
+            file = await self.ipfs.get(hash)
+            decompressed = zlib.decompress(file)
+            decrypted = self.creolang.execute("decrypt.cl", decompressed)
 
-        self.cache.set(path, decrypted)
-        return decrypted
+            self.cache.set(path, decrypted)
+            return decrypted
+        except Exception as e:
+            self.logger.log_error(f"Retrieve operation failed: {e}")
+            return None
 
     def initialize_storage_nodes(self):
-        # Bootstrap DHT routing
+        # Bootstrap DHT routing logic
 
     async def migrate_data_to_filecoin(self):
-        # Logic to migrate data from IPFS to Filecoin based on access patterns and benchmarks
+        # Logic to migrate data from IPFS to Filecoin
 
     def log_metrics(self):
-        # Log usage and performance metrics
+        # Implement metric logging functionality
 
-# Example usage
+# Example usage (this should be executed in an asynchronous context)
 storage = DecentralizedCloudService()
 storage.initialize()
 
-data = "hello world"
-await storage.store(data, "file.txt", use_filecoin=True)
-retrieved = await storage.retrieve("file.txt")
+async def run_storage_operations():
+    data = "hello world"
+    await storage.store(data, "file.txt", use_filecoin=True)
+    retrieved = await storage.retrieve("file.txt")
+
+asyncio.run(run_storage_operations())
