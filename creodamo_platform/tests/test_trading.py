65c7bc0d81e1bc02f3c6a48f99fc630d37239d37
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-20 16:50:44 -0500

    Update test_trading.py
    git update test_trading.py

--- creodamo_platform/tests/test_trading.py
+++ creodamo_platform/tests/test_trading.py
@@ -1,4 +1,38 @@
-from creodamo_platform.trading import TradingBot 
+import unittest
+import locust
+from creodamo_platform.trading import TradingBot
+import market_data_simulator
 
-def test_trading_bot_execution():
-   pass
+class TestTradingBotPerformance(unittest.TestCase):
+
+    def test_trading_bot_execution(self):
+        # Test trading bot under normal conditions
+        bot = TradingBot()
+        bot.execute_trading_logic()
+        # Add assertions as needed
+
+    def test_market_data_simulation(self):
+        # Inject simulated market data and test trading bot's response
+        bot = TradingBot()
+        simulated_data = market_data_simulator.generate_data()
+        bot.process_market_data(simulated_data)
+        # Add assertions for bot's strategy logic
+
+    def test_trade_execution_latency(self):
+        # Simulate trade execution latencies
+        bot = TradingBot()
+        bot.set_latency_simulation(True)
+        bot.execute_trading_logic()
+        # Verify bot's performance under latency conditions
+
+    # Additional load testing using Locust
+    class TradingBotUser(locust.HttpUser):
+        @locust.task
+        def execute_trade(self):
+            self.trading_bot = TradingBot()
+            self.trading_bot.execute_trading_logic()
+            # Verify trade execution under load
+
+# Run Locust load test if this script is executed directly
+if __name__ == "__main__":
+    locust.env.create_environment(user_classes=[TestTradingBotPerformance.TradingBotUser])
