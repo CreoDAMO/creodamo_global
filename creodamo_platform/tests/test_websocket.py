21bb668a41827a4d326ae455505bd5c3d7ea344a
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2023-12-20 16:53:09 -0500

    Update test_websocket.py
    git update test_websocket.py

--- creodamo_platform/tests/test_websocket.py
+++ creodamo_platform/tests/test_websocket.py
@@ -1,14 +1,32 @@
+import unittest
+import locust
 import websockets
 import asyncio
-import unittest
+import json
 
 class TestWebSocket(unittest.TestCase):
 
-  async def test_connect(self):
-    async with websockets.connect("http://localhost:8765") as ws:  
-      response = await ws.recv()
-      data = json.loads(response)
-      self.assertEqual(data["key"], "value")
+    async def test_connect(self):
+        async with websockets.connect("ws://localhost:8765") as ws:
+            await ws.send("Test Message")
+            response = await ws.recv()
+            data = json.loads(response)
+            self.assertEqual(data["key"], "value")
 
+    # Additional tests for error handling and edge conditions
+    async def test_error_handling(self):
+        # Simulate WebSocket errors and validate recovery logic
+        # ...
+
+    # Load testing using Locust
+    class WebSocketUser(locust.HttpUser):
+        @locust.task
+        async def connect_and_send(self):
+            async with websockets.connect("ws://localhost:8765") as ws:
+                await ws.send("Test Message")
+                await ws.recv()
+                # Validate WebSocket performance under high connection load
+
+# Run Locust load test if this script is executed directly
 if __name__ == "__main__":
-  unittest.main()
+    locust.env.create_environment(user_classes=[TestWebSocket.WebSocketUser])
